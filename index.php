df53595de929293f83fc019fa4500f047e7fe3e5
Savour <savour@Mac-mini-de-Savour.local>
2025-05-06 16:56:40 +0200

    third upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> <!-- Importación CDN Tailwind -->
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="src/controllers/auth/form-register.php">Regístrarse</a>
+    <a href="src/controllers/auth/form-login.php">Iniciar sesión</a>
+    <?php include 'src/header.php'; ?> <!-- plantilla header-->
+    <main>
+        <!-- contenido principal -->
+    </main>
+    <?php include 'src/footer.php'; ?> <!-- plantilla footer -->
+
+</body>
+
+</html>
---
+++ src/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-700 p-4 flex items-center justify-center">
+  <!-- Usar un SVG sprite permite agrupar todos los iconos en un único archivo que se carga solo una vez. -->
+
+<div>  <img src="/zermatt-uhren/public/img/branding/logotipo/logo_nbk.png" alt="Logo de la empresa" class="h-20 w-20">
+</div>
+<div></div>
+<div></div> 
+</footer>
